96c783b2c29182940fb877eb70fe56f3e633299a
drewlauby <42359145+drewlauby@users.noreply.github.com>
2022-04-12 17:57:44 -0500

    Added Neo4j connection class and way to load the database

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
+from neo4j import GraphDatabase
 
+class Neo4jConnection:
+    
+    def __init__(self, uri, user, pwd):
+        self.__uri = uri
+        self.__user = user
+        self.__pwd = pwd
+        self.__driver = None
+        try:
+            self.__driver = GraphDatabase.driver(self.__uri, auth=(self.__user, self.__pwd))
+        except Exception as e:
+            print("Failed to create the driver:", e)
+        
+    def close(self):
+        if self.__driver is not None:
+            self.__driver.close()
+        
+    def query(self, query, db=None):
+        assert self.__driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try: 
+            session = self.__driver.session(database=db) if db is not None else self.__driver.session() 
+            response = list(session.run(query))
+        except Exception as e:
+            print("Query failed:", e)
+        finally: 
+            if session is not None:
+                session.close()
+        return response
+
+conn = Neo4jConnection(uri="bolt://localhost:7687", user="basketball", pwd="retrieval")
+
+conn.query("CREATE OR REPLACE DATABASE basketball")
+
+query_string = '''
+LOAD CSV WITH HEADERS FROM
+'file:///basketball.csv'
+AS line FIELDTERMINATOR ','
+CREATE (:Paper {id: line.paper_id, class: line.label})
+'''
+
+conn.query(query_string, db='basketball')
+
+query_string = '''
+LOAD CSV WITH HEADERS FROM
+'file:///basketball.csv'
+AS line FIELDTERMINATOR ','
+MATCH (citing_paper:Paper {id: line.citing_paper_id}),(cited_paper:Paper {id: line.cited_paper_id})
+CREATE (citing_paper)-[:CITES]->(cited_paper)
+'''
+conn.query(query_string, db='basketball')
