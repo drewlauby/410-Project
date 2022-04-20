783009cea749e76c4d04f40e97ac865d3061d1c5
drewlauby <42359145+drewlauby@users.noreply.github.com>
2022-04-20 14:56:10 -0500

    Added data files, way to populate database

--- main.py
+++ main.py
@@ -30,24 +30,64 @@ class Neo4jConnection:
                 session.close()
         return response
 
-conn = Neo4jConnection(uri="bolt://localhost:7687", user="basketball", pwd="retrieval")
+conn = Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", pwd="retrieval")
 
-conn.query("CREATE OR REPLACE DATABASE basketball")
+# conn.query("CREATE OR REPLACE DATABASE basketball")
 
+#create player nodes
 query_string = '''
-LOAD CSV WITH HEADERS FROM
-'file:///basketball.csv'
-AS line FIELDTERMINATOR ','
-CREATE (:Paper {id: line.paper_id, class: line.label})
+LOAD CSV WITH HEADERS FROM 'file:///Player_Attributes.csv' AS row
+MERGE (player:Player {playerID: row.ID})
+ON CREATE SET player.firstName = row.FIRST_NAME, player.lastName = row.LAST_NAME,
+player.birthDate = row.BIRTHDATE, player.school = row.SCHOOL, player.country = row.COUNTRY,
+player.height = row.HEIGHT, player.weight = row.WEIGHT, player.seasonsPlayed = row.SEASON_EXP,
+player.jerseyNumber = row.JERSEY, player.position = row.POSITION, 
+player.roundDrafted = row.DRAFT_ROUND, player.pickDrafted = row.DRAFT_NUMBER, 
+player.pointsPerGame = row.PTS, player.assistsPerGame = row.AST, 
+player.reboundsPerGame = row.REB
 '''
+conn.query(query_string, db='neo4j')
 
+#set player names
+query_string = '''
+match(p:Player) set p.name = p.firstName + ' ' + p.lastName
+'''
+conn.query(query_string, db='neo4j')
+
+#create team nodes
+query_string = '''
+LOAD CSV WITH HEADERS FROM 'file:///Team_History.csv' AS row
+MERGE (team:Team {nickname: row.NICKNAME, city: row.CITY, yearFounded: row.YEARFOUNDED})
+ON CREATE SET team.teamID = row.ID, team.yearFounded = row.YEARFOUNDED,
+team.yearActiveTill = row.YEARACTIVETILL
+'''
+conn.query(query_string, db='neo4j')
+
+#set team names
+query_string = '''
+match(t:Team) set t.name = t.city + ' ' + t.nickname
+'''
 conn.query(query_string, db='basketball')
 
+#add team attributes
 query_string = '''
-LOAD CSV WITH HEADERS FROM
-'file:///basketball.csv'
-AS line FIELDTERMINATOR ','
-MATCH (citing_paper:Paper {id: line.citing_paper_id}),(cited_paper:Paper {id: line.cited_paper_id})
-CREATE (citing_paper)-[:CITES]->(cited_paper)
+LOAD CSV WITH HEADERS FROM 'file:///Team_Attributes.csv' AS row
+MATCH (team:Team) WHERE team.teamID = row.ID AND team.yearActiveTill = '2019' SET team.abbreviation = row.ABBREVIATION,
+team.arena = row.ARENA, team.arenaCapacity = row.ARENACAPACITY, team.owner = row.OWNER, team.gm = row.GENERALMANAGER,
+team.coach = row.HEADCOACH, team.dLeague = row.DLEAGUEAFFILIATION
 '''
-conn.query(query_string, db='basketball')
+conn.query(query_string, db='neo4j')
+
+#create played for relationships
+query_string = '''
+LOAD CSV WITH HEADERS FROM 'file:///Player_Bios.csv' AS row
+MATCH (p:Player), (t:Team) WHERE p.name = row.namePlayerBREF and t.name = row.nameTeam
+create (p)-[r:Played_For {year: row.slugSeason}]->(t)
+'''
+
+result = conn.query(query_string, db='neo4j')
+
+print(result)
+
+
+
